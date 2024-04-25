a53c24449387e6870ad542790f073a28d65e761c
“neyser-eviden” <neyser.huaman.external@eviden.com>
2024-04-25 16:09:48 +0200

    Uso de Input (Componente Padre a Hijo)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { EmpleadoHijoCComponent } from './empleado-hijo-c/empleado-hijo-c.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpleadoHijoCComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/empleado-hijo-c/empleado-hijo-c.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Empleado } from '../empleado.model';
+
+@Component({
+  selector: 'app-empleado-hijo-c',
+  templateUrl: './empleado-hijo-c.component.html',
+  styleUrl: './empleado-hijo-c.component.css'
+})
+export class EmpleadoHijoCComponent {
+
+  @Input() empleadoPadre:Empleado;
+  @Input() iPadre:number;
+
+}
