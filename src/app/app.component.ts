058fb5ef0cb70c23bc917b2116a4d51718c2884d
“neyser-eviden” <neyser.huaman.external@eviden.com>
2024-04-25 13:56:48 +0200

    Agregando formulario y funcionalidad de agregar nuevos empleados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'empleadosApp';
+  titulo = 'Listado de Empleados';
+
+
+  empleados: Empleado[] = [
+    new Empleado("Juan", "Perez", "Gerente", 4500),
+    new Empleado("Jose", "Garcia", "Jefe", 4000),
+    new Empleado("Maria", "Lopez", "Asistente", 2500),
+    new Empleado("Neyser", "Huaman", "Asistente", 3500)
+  ];
+
+  valNombre: string = "";
+  valApellido: string = "";
+  valCargo: string = "";
+  valSalario: number = 0;
+
+  agregarEmpleado(){
+    let empleado = new Empleado(this.valNombre, this.valApellido, this.valCargo, this.valSalario);
+    this.empleados.push(empleado);
+
+    this.valNombre="";
+    this.valApellido="";
+    this.valCargo="";
+    this.valSalario=0;
+  }
+  
+  
+
 }
